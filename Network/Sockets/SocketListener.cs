486d0fdc30f9b327171d64b3aeaaef31c498ab0f
NEERAJ KRISHNA N <neerajnjps03@gmail.com>
2024-11-14 18:56:08 +0530

    changed Network modules code to the previous version

--- Network/Sockets/SocketListener.cs
+++ Network/Sockets/SocketListener.cs
@@ -181,25 +181,18 @@ namespace Networking.Sockets
                 // get the packet string from the received string and
                 // convert it back to packet and enqueue the packet
                 // to receiving queue
-                try
-                {
-                    string packetString = receivedString[
-                        packetBegin..(packetEnd + 6)];
-                    Packet packet = PacketString.PacketStringToPacket(
-                        packetString);
-                    _receivingQueue.Enqueue(packet);
+                string packetString = receivedString[
+                    packetBegin..(packetEnd + 6)];
+                Packet packet = PacketString.PacketStringToPacket(
+                    packetString);
+                _receivingQueue.Enqueue(packet);
 
-                    // remove the first packet from the received string
-                    receivedString = receivedString[(packetEnd + 6)..];
+                // remove the first packet from the received string
+                receivedString = receivedString[(packetEnd + 6)..];
 
-                    Trace.WriteLine("[Networking] SocketListener:" +
-                        " Received data from module: "
-                        + packet.moduleOfPacket);
-                }
-                catch (Exception ex)
-                {
-                    Trace.WriteLine($"[Networking] (FileClonerModification) {ex.Message}");
-                }
+                Trace.WriteLine("[Networking] SocketListener:" +
+                    " Received data from module: "
+                    + packet.moduleOfPacket);
             }
             Trace.WriteLine("[Networking] SocketListener." +
                 "ProcessReceivedString() function exited.");
